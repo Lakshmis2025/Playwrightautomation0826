f5c697c219883977b1e004fb9fd6f988d149c0f1
Lakshmis2025 <lakshmisurisetti2025@gmail.com>
2026-08-20 23:07:14 -0400

    PlayWright Test Hook

---
+++ tests/AssignmentsTest/3PlaywrightTestHook2.spec.js
@@ -0,0 +1,17 @@
+/*Login into the OrangeHRMS
+Get the name of the user
+Print the console area
+Perform Logout*/
+
+import{test, expect} from '@playwright/test';
+test('Login,name of the user,Print,Logout',async ({page}) => {
+    await page.goto('/');
+    await page.getByPlaceholder('username').fill('Admin');
+    await page.getByPlaceholder('password').fill('admin123');
+    await page.getByRole('button', {name:'Login'}).click();
+    const NameOfTheUser = await page.locator('.oxd-userdropdown-name').textContent();
+    //page.locator('.class-name')..finds the element that displays the logged-in user's name.
+    //textContent();Gets the text inside that element.
+    console.log(NameOfTheUser);
+    await page.getByRole('button', {name:'Logout'}).click();
+});
