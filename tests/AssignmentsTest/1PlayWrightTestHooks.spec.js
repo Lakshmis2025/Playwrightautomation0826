e27f077719b70850ac5abc9039f49af25e74f085
Lakshmis2025 <lakshmisurisetti2025@gmail.com>
2026-08-21 09:55:20 -0400

    1PlayWrightTestHooks

---
+++ tests/AssignmentsTest/1PlayWrightTestHooks.spec.js
@@ -0,0 +1,32 @@
+/*Login into OrangeHRMS-
+Assert the following elements is visible or not
+Time at work
+MyActions
+Quick Launch
+Buzz Latest Posts
+Employees on Leave Today
+Employee Distribution by Sub Unit*/
+
+import { test, expect } from '@playwright/test';
+
+test('Assert the dashboard elements are visible or not', async ({ page }) => {
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+await page.getByPlaceholder('username').fill('Admin');
+await page.getByPlaceholder('password').fill('admin123');
+await page.getByRole('button', { name: 'Login' }).click();
+
+/*getByText('Quick Launch')	Find text containing Quick Launch
+getByText('Quick Launch', { exact: true })	Find text exactly equal to Quick Launch */
+await expect(page.getByText('Time at Work', { exact: true })).toBeVisible();
+await expect(page.getByText('My Actions', { exact: true })).toBeVisible();
+await expect(page.getByText('Quick Launch', { exact: true })).toBeVisible();
+await expect(page.getByText('Buzz Latest Posts', { exact: true })).toBeVisible();
+await expect(page.getByText('Employees on Leave Today', { exact: true })).toBeVisible();
+await expect(page.getByText('Employee Distribution by Sub Unit', { exact: true })).toBeVisible();
+});
+
+
+
+
+
+
